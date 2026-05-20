25481ccf123e7669c190a18d7b4fa70a4b980c4b
Prosun Kumar Das <remodas299@gmail.com>
2026-05-20 12:54:03 +0530

    Update Flask app to use environment port

--- backend/app.py
+++ backend/app.py
@@ -193,4 +193,6 @@ if __name__ == '__main__':
     print("  CD-RPS Flask Backend + Frontend")
     print("  Open: http://127.0.0.1:5000")
     print("="*55 + "\n")
-    app.run(debug=True, port=5000)
+   if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
